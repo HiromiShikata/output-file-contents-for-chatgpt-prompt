eba239153e7612522c893c9d01e49797804d90e2
HiromiShikata <shikata.hiromi@gmail.com>
2023-03-25 20:32:12 +0000

    chore: add interface to run as cli

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,15 @@
-import { hello } from './index';
+import { execSync } from 'child_process';
 
-describe('index', () => {
-  test('hello', async () => {
-    const res = hello('test');
-    expect(res).toEqual('hello test');
+describe('commander program', () => {
+  it('should output input path', () => {
+    const output = execSync(
+      'npx ts-node ./src/index.ts -i ./test/data/file.txt',
+    ).toString();
+    expect(output.trim()).toBe('input: ./test/data/file.txt');
+  });
+
+  it('should not output anything without input path', () => {
+    const output = execSync('npx ts-node ./src/index.ts').toString();
+    expect(output.trim()).toBe('--input parameter is required.');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export const hello = (name: string) => `hello ${name}`;
+import { Command } from 'commander';
+
+const program = new Command();
+program
+  .description('Output file contents with file path for ChatGPT prompt.')
+  .option(`-i, --input <path>`, 'Path of input file or directory.')
+  .action(({ input }: { input: string }) => {
+    if (!input) {
+      console.log(`--input parameter is required.`);
+      return;
+    }
+    console.log(`input: ${input}`);
+  });
+program.parse(process.argv);
