0594de37385b1418bf1e700f13a58d3c88fbe5f8
HiromiShikata <shikata.hiromi@gmail.com>
2023-03-25 21:29:44 +0000

    fix: add shabangs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { Command } from 'commander';
 import { GetFileContentsTextUseCase } from './domain/usecases/GetFileContentsTextUseCase';
 import { FsFileRepository } from './adapter/repositories/FsFileRepository';
