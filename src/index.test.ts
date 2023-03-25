6cc3c2533f9f32c43adcfd1071e1e56bd2927b2a
HiromiShikata <shikata.hiromi@gmail.com>
2023-03-25 21:16:28 +0000

    feat: connect usecacse to entrypoint

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,23 @@
 import { execSync } from 'child_process';
 
 describe('commander program', () => {
-  it('should output input path', () => {
+  it('should output file contents', () => {
     const output = execSync(
-      'npx ts-node ./src/index.ts -i ./test/data/file.txt',
+      'npx ts-node ./src/index.ts -i ./src/adapter/repositories/testdata',
     ).toString();
-    expect(output.trim()).toBe('input: ./test/data/file.txt');
+    expect(output.trim()).toBe(`// src/adapter/repositories/testdata/File1.txt
+Content of File1.txt
+
+
+// src/adapter/repositories/testdata/File2.txt
+Content of File2.txt
+
+
+// src/adapter/repositories/testdata/dir1/File1.txt
+Content of File1.txt`);
   });
 
-  it('should not output anything without input path', () => {
+  it('should output error message without input path', () => {
     const output = execSync('npx ts-node ./src/index.ts').toString();
     expect(output.trim()).toBe('--input parameter is required.');
   });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { Command } from 'commander';
+import { GetFileContentsTextUseCase } from './domain/usecases/GetFileContentsTextUseCase';
+import { FsFileRepository } from './adapter/repositories/FsFileRepository';
 
 const program = new Command();
 program
   .description('Output file contents with file path for ChatGPT prompt.')
   .option(`-i, --input <path>`, 'Path of input file or directory.')
-  .action(({ input }: { input: string }) => {
+  .action(async ({ input }: { input: string }) => {
     if (!input) {
       console.log(`--input parameter is required.`);
       return;
     }
-    console.log(`input: ${input}`);
+    const fileContentsText = await new GetFileContentsTextUseCase(
+      new FsFileRepository(),
+    ).run(input);
+    console.log(fileContentsText);
   });
 program.parse(process.argv);
